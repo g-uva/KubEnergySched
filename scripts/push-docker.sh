bd2ad9d5d72382b222adc19f5e5d5c100e7489ab
Gonçalo <g.j.teixeiradepinhoferreira@uva.nl>
2025-06-05 16:05:56 +0200

    Fixing some issues with the pods initialisation.

--- scripts/push-docker.sh
+++ scripts/push-docker.sh
@@ -2,12 +2,16 @@
 
 # I am saving the token in an .env file for security reasons.
 # The token should expire after 90 days.
-GHCR_TOKEN=$(cat .env/GHCR_TOKEN)
+set -a 
+source .env
+set +a
 if [ -z "$GHCR_TOKEN" ]; then
   echo "Error: GHCR_TOKEN is not set in .env/GHCR_TOKEN"
   exit 1
 fi
 echo $GHCR_TOKEN | docker login ghcr.io -u g-uva --password-stdin
 
-docker build -t ghrc.io/g-uva/centralunit:latest ../manifest/templates/centralunit-deployment.yaml
-docker build -t ghrc.io/g-uva/compute-node:latest ../manifest/templates/compute-node.yaml
+pwd
+
+docker build -t ghcr.io/g-uva/centralunit:latest ./manifest/templates/centralunit-deployment.yaml
+docker build -t ghcr.io/g-uva/compute-node:latest ./manifest/templates/compute-node.yaml
