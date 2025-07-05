db114100692c7c10c277ffb8221147192a658442
Gonçalo <g.j.teixeiradepinhoferreira@uva.nl>
2025-07-05 22:46:57 +0200

    Fixing namespace and centralunit Go file.

--- scripts/restart_helm.sh
+++ scripts/restart_helm.sh
@@ -3,5 +3,6 @@
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 cd $SCRIPT_DIR/../helm/
 # Restart Helm release with `eu-cluster`.
-# helm upgrade --install eu-cluster . -n eu-central --recreate-pods
-helm upgrade --install eu-cluster . -n eu-central --recreate-pods --create-namespace
+helm upgrade --install eu-cluster . -n eu-central --create-namespace
+kubectl rollout restart statefulset compute -n eu-central
+kubectl rollout restart deployment centralunit -n eu-central
