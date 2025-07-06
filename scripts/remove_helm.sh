05f5cda2588e2cf2f40075a7158cdd86e5142a69
Gonçalo <g.j.teixeiradepinhoferreira@uva.nl>
2025-07-06 18:21:30 +0200

    PodMonitor installed successfully. Fixing Scaphandre fallback_scrape_protocol next.

--- scripts/remove_helm.sh
+++ scripts/remove_helm.sh
@@ -3,7 +3,10 @@
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 cd $SCRIPT_DIR/../helm/
 
-helm uninstall eu-cluster -n eu-central
+kubectl patch pv external-pv -p '{"metadata":{"finalizers": []}}' --type=merge
+kubectl delete podmonitor compute-scaphandre -n eu-central
 kubectl delete pv external-pv
+helm uninstall eu-cluster -n eu-central
+helm uninstall eu-monitoring -n eu-central
 
 echo "eu-cluster removed successfully :)"
